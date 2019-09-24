871dbb687ddfcf2fc7f6bd9f0b8c55782c9ef8b8
Felipe Hernández <feehernandezba@gmail.com>
2019-09-23 22:13:03 -0500

    Fixed faces and controllers for sales

--- src/java/SQL/flowersx.sql
+++ src/java/SQL/flowersx.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 11-09-2019 a las 18:37:44
+-- Tiempo de generación: 24-09-2019 a las 05:09:18
 -- Versión del servidor: 10.1.38-MariaDB
 -- Versión de PHP: 7.3.2
 
@@ -141,6 +141,17 @@ CREATE TABLE `pedido` (
   `Usuario_id` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Volcado de datos para la tabla `pedido`
+--
+
+INSERT INTO `pedido` (`idPedido`, `fechaDeCreacion`, `fechaDeEntrega`, `subTotal`, `total`, `Usuario_id`) VALUES
+(1, '2019-09-22', '2019-12-22', 250000, 300000, 12),
+(2, '2019-09-23', '2019-09-23', 0, 0, 12),
+(3, '2019-09-23', '2019-09-23', 0, 0, 12),
+(4, '2019-09-23', '2019-09-23', 0, 0, 12),
+(5, '2019-09-23', '2019-09-23', 0, 0, 12);
+
 -- --------------------------------------------------------
 
 --
@@ -172,11 +183,10 @@ INSERT INTO `permisos` (`idpermisos`, `nombre`, `nombre_en`, `url`, `icon`, `per
 (9, 'Consultar órdenes de producción', 'Search production orders', 'Admin/gestionar-ordenProduccion-admin.xhtml', 'x', 3),
 (10, 'Crear Novedad', 'Create novelty', 'Admin/crear-novedad.xhtml', 'x', 3),
 (11, 'Gestionar Novedades', 'Manage novelties', 'Admin/gestionar-novedades.xhtml', 'x', 3),
-(12, 'Crear item de catálogo', 'Create catalog item', 'Admin/crear-catalogo.xhtml', 'x', 4),
+(12, 'Registrar producto', 'Add product', 'Admin/crear-catalogo.xhtml', 'x', 4),
 (13, 'Gestionar Catálogo', 'Manage catalog', 'Admin/gestionar-catalogo.xhtml', 'x', 4),
 (14, 'Nueva orden de producción', 'Create production order', 'Ingeniero/crear-ordenProduccion.xhtml', 'x', 3),
 (15, 'Gestionar órdenes de producción', 'Manage production orders', 'Ingeniero/gestionar-ordenProduccion.xhtml', 'x', 3),
-(16, 'Consultar pedidos', 'Search sales orders', 'Ingeniero/gestionar-pedido-ingeniero.xhtml', 'x', 3),
 (17, 'Ventas', 'Sales', '', '', NULL),
 (18, 'Nuevo pedido', 'New sales order', 'Vendedor/crear-pedido-vendedor.xhtml', 'x', 17),
 (19, 'Consultar pedidos', 'Search sales orders', 'Vendedor/gestionar-pedido-vendedor.xhtml', 'x', 17),
@@ -185,7 +195,10 @@ INSERT INTO `permisos` (`idpermisos`, `nombre`, `nombre_en`, `url`, `icon`, `per
 (22, 'Gestionar métodos de pago', 'Manage payment methods', 'Cliente/gestionar-pago.xhtml', 'x', 20),
 (23, 'Comprar', 'Buy', NULL, NULL, NULL),
 (24, 'Nuevo pedido', 'New order', 'Cliente/crear-pedido.xhtml', 'x', 23),
-(25, 'Consultar pedidos', 'Search orders', 'Cliente/gestionar-pedido.xhtml', 'x', 23);
+(25, 'Consultar pedidos', 'Search orders', 'Cliente/gestionar-pedido.xhtml', 'x', 23),
+(26, 'Consultar catálogo', 'Catalog lookup', 'Cliente/consultar-catalogo.xhtml', 'x', 4),
+(27, 'Consultar pedidos', 'Search orders', 'Admin/consultar-pedidos.xhtml', 'x', 3),
+(28, 'Consultar pedidos', 'Search orders', 'Ingeniero/consultar-pedidos.xhtml', 'x', 3);
 
 -- --------------------------------------------------------
 
@@ -213,7 +226,8 @@ INSERT INTO `producto` (`idProducto`, `nombreProducto`, `foto`, `descripcion`, `
 (3, 'Girasoles', '/FlowersXProyecto/Archivos/sunflower.jpg', 'Varios colores disponibles', 3, 80000, 150),
 (4, 'Tulipanes', '/FlowersXProyecto/Archivos/tulip.jpg', 'Varios colores disponibles', 4, 0, 250),
 (5, 'Azucenas', '/FlowersXProyecto/Archivos/azucena.jpg', 'Varios colores disponibles.', 4, 500, 300),
-(6, 'Violetas', '/FlowersXProyecto/Archivos/violeta.jpg', 'Disponible en un solo color.', 2, 0, 250);
+(6, 'Violetas', '/FlowersXProyecto/Archivos/violeta.jpg', 'Disponible en un solo color.', 2, 0, 250),
+(7, 'Claveles', '/FlowersXProyecto/Archivos/flor-clavel.jpg', 'Varios colores disponibles', 4, 600, 100);
 
 -- --------------------------------------------------------
 
@@ -225,6 +239,17 @@ CREATE TABLE `producto_has_pedido` (
   `producto_idProducto` int(5) NOT NULL,
   `pedido_idPedido` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `producto_has_pedido`
+--
+
+INSERT INTO `producto_has_pedido` (`producto_idProducto`, `pedido_idPedido`) VALUES
+(1, 1),
+(2, 1),
+(3, 1),
+(5, 1),
+(7, 1);
 
 -- --------------------------------------------------------
 
@@ -278,6 +303,7 @@ INSERT INTO `rol_has_permisos` (`Rol_idRol`, `permisos_idpermisos`) VALUES
 (1, 11),
 (1, 12),
 (1, 13),
+(1, 27),
 (2, 2),
 (2, 3),
 (2, 7),
@@ -285,19 +311,21 @@ INSERT INTO `rol_has_permisos` (`Rol_idRol`, `permisos_idpermisos`) VALUES
 (2, 9),
 (2, 10),
 (2, 11),
+(2, 28),
 (3, 3),
 (3, 14),
 (3, 15),
-(3, 16),
 (4, 17),
 (4, 18),
 (4, 19),
+(5, 4),
 (5, 20),
 (5, 21),
 (5, 22),
 (5, 23),
 (5, 24),
-(5, 25);
+(5, 25),
+(5, 26);
 
 -- --------------------------------------------------------
 
@@ -349,17 +377,16 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`id`, `titular`, `razonSocial`, `email`, `password`, `estado`, `Rol_idRol`, `pais_idpais`, `ciudad_idciudad`) VALUES
-(1, 'Felipe Hernández', 'FlowersX', 'feehernandezba@gmail.com', '25f9e794323b453885f5181f1b624d0b', 1, 1, 8, 1),
-(2, 'Shirley Bernal', 'SENA', 'stbernal0@misena.edu.co', 'zpRv7BHhkK', 0, 5, 8, 1),
+(1, 'Felipe Hernández', 'FlowersX', 'feehernandezba@gmail.com', 'a873de377ffbd389c31e5fc9f5c6dcd8', 1, 1, 8, 1),
 (3, 'Santiago Ruiz', 'SENA', 'sruiz8864@misena.edu.co', 'XRO31ZLzk9', 0, 5, 8, 1),
 (4, 'Sergio Quintero', 'SENA', 'saquintero074@misena.edu.co', '7qLhDRZEye', 0, 5, 8, 1),
 (5, 'Jonathan Patiño', 'SENA', 'japatino967@misena.edu.co', 'ge4lyDur5H', 0, 5, 8, 1),
 (6, 'Santiago Reyes', 'SENA', 'sreyes98@misena.edu.co', '123456789', 0, 5, 8, 1),
 (7, 'Santiago Salinas', 'SENA', 'dssalinas5@misena.edu.co', 'gv27tbNduB', 0, 5, 8, 1),
 (8, 'Diego Arenas', 'SENA', 'daarenas83@misena.edu.co', 'Ec1x6m4Sjv', 0, 5, 8, 1),
-(9, 'Valentina Marín', 'SENA', 'dvmarin3@misena.edu.co', 'SGnCP5rBgf', 0, 5, 8, 1),
-(10, 'Juan Pablo Rodríguez', 'FlowersX', 'jprodriguez744@misena.edu.co', 'bFMCp709KN', 0, 2, 8, 1),
-(11, 'Jerson Chitán', 'FlowersX', 'jachitan@misena.edu.co', 'lqhmMRr0UT', 0, 3, 8, 1);
+(9, 'Valentina Marín', 'SENA', 'dvmarin3@misena.edu.co', 'SGnCP5rBgf', 0, 1, 8, 1),
+(12, 'Shirley Bernal', 'SENA', 'stbernal0@misena.edu.co', '25f9e794323b453885f5181f1b624d0b', 0, 5, 8, 1),
+(14, 'Juan Pablo Rodríguez', 'SENA', 'jprodriguez744@misena.edu.co', '0ed48cf6dc34f1f72a141a8014545629', 0, 3, 8, 1);
 
 --
 -- Índices para tablas volcadas
@@ -498,13 +525,13 @@ ALTER TABLE `pais`
 -- AUTO_INCREMENT de la tabla `pedido`
 --
 ALTER TABLE `pedido`
-  MODIFY `idPedido` int(5) NOT NULL AUTO_INCREMENT;
+  MODIFY `idPedido` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `producto`
 --
 ALTER TABLE `producto`
-  MODIFY `idProducto` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `idProducto` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `rol`
@@ -522,7 +549,7 @@ ALTER TABLE `solicitud`
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- Restricciones para tablas volcadas
@@ -579,13 +606,6 @@ ALTER TABLE `producto_has_pedido`
 ALTER TABLE `rol_has_permisos`
   ADD CONSTRAINT `fk_Rol_has_permisos_Rol1` FOREIGN KEY (`Rol_idRol`) REFERENCES `rol` (`idRol`),
   ADD CONSTRAINT `fk_Rol_has_permisos_permisos1` FOREIGN KEY (`permisos_idpermisos`) REFERENCES `permisos` (`idpermisos`);
-
---
--- Filtros para la tabla `solicitud`
---
-ALTER TABLE `solicitud`
-  ADD CONSTRAINT `fk_Solicitud_Pedido1` FOREIGN KEY (`Pedido_idPedido`) REFERENCES `pedido` (`idPedido`),
-  ADD CONSTRAINT `fk_Solicitud_Usuario1` FOREIGN KEY (`Usuario_id`) REFERENCES `usuario` (`id`);
 
 --
 -- Filtros para la tabla `usuario`
